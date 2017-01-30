586dbfe6308d1258d1f1751bfbbcb3ee05ecf0d1
Pierre Tardy <tardyp@gmail.com>
2017-01-30 14:34:20 +0100

    better manage master

--- dostress.sh
+++ dostress.sh
@@ -1,13 +1,16 @@
 # -l/bin/bash
 #python marathon.py master 1
 #sleep 300
-for k in 0.005 0.01
+for masters in 1 2 3 4
 do
-for i in 10000 40000 100
+for sleeptime in 0 
 do
-for j in 100 200 300 
+for numlines in 10000 40000 100
 do
-python stressmarathon.py 300 $j 4pypy $i $k || exit
+for workers in 100 200 300 
+do
+python stressmarathon.py 300 $workers $masters pypy $numlines $sleeptime || exit
+done
 done
 done
 done
